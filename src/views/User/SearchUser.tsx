8372758eece25864bd88a34bb46e39d7641927a6
Hiep Nguyen <nguyenhieptech@gmail.com>
2022-02-27 20:59:26 +0700

    Update search but I haven't finished yet

--- src/views/User/SearchUser.tsx
+++ src/views/User/SearchUser.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
+import PaginatedQuery from './PaginatedQuery';
 
 function SearchUser() {
-  return <div>This is search User. I haven't implemented it yet.</div>;
+  const [searchQuery, setSearchQuery] = useState('');
+
+  function handleChange(e: any) {
+    setSearchQuery(e.target.value);
+  }
+
+  return (
+    <>
+      <input type="text" value={searchQuery} onChange={handleChange} />
+      This is search User. I haven't implemented it yet.
+      {/* <PaginatedQuery /> */}
+    </>
+  );
 }
 
 export default SearchUser;
